486a293a15df539865c575b1cedecedbd4fbe3bf
Moorthi Mahalingam <moorthitechtrain@gmail.com>
2016-07-07 01:14:28 -0400

    restaurant update details code has been added

--- restaurent_registration/src/main/java/com/gogenie/restaurent/registration/controller/RestraurentRegistrationController.java
+++ restaurent_registration/src/main/java/com/gogenie/restaurent/registration/controller/RestraurentRegistrationController.java
@@ -51,6 +51,7 @@ public class RestraurentRegistrationController {
 		return response;
 	}
 
+	
 	@ExceptionHandler(RestaurentRegistrationException.class)
 	public String exceptionHandler(RestaurentRegistrationException exception) {
 		return null;
